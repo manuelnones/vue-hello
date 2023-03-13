0c4cf1095d3b0f8db228fc29e15442932a37d9c3
manuelnones <manuelnones99@yahoo.com>
2023-03-13 17:00:27 +0100

    add bonus

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const { createApp } = Vue
 createApp({
     data() {
       return {
-        message: 'Titolo con vue!',
+        message: `Titolo con vue!`,
         image: `https://www.geekandjob.com/uploads/wiki/f236a1aa005ef9a93041f294749e86d1.png`
       }
     }
