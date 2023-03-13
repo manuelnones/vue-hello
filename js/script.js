48f2c4f32f9e185a8ae43a0e88e3f27037d39ce4
manuelnones <manuelnones99@yahoo.com>
2023-03-13 16:53:12 +0100

    add exercise

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+      return {
+        message: 'Titolo con vue!',
+        image: `https://www.geekandjob.com/uploads/wiki/f236a1aa005ef9a93041f294749e86d1.png`
+      }
+    }
+  }).mount('#app')
